028b969e006b5e297e77870081ef9a0db6f6bbac
oikiyuto <nohappy_noyuto@yahoo.co.jp>
2019-02-02 19:45:37 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by yutos on 2019/02/02.
+//  Copyright © 2019 yutos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
